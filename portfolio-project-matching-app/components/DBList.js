05efb5d3c75ac4cfff41fb2640b2925595a4e35b
Logan Kiser <kiserd@oregonstate.edu>
2021-10-06 17:25:15 -0700

    tinkering with tailwind and nested components

--- portfolio-project-matching-app/components/DBList.js
+++ portfolio-project-matching-app/components/DBList.js
@@ -2,7 +2,7 @@ import DBListItem from './DBListItem'
 
 const DBList = ({ docList }) => {
     return (
-        <div>
+        <div className='flex-row flex-wrap justify-start max-w-sm max-h-lg'>
             {docList.map((doc) => {
                 return <DBListItem key={doc.id} doc={doc} />
             })}
--- portfolio-project-matching-app/components/DBListItem.js
+++ portfolio-project-matching-app/components/DBListItem.js
@@ -1,6 +1,6 @@
 const DBListItem = ({ doc }) => {
     return (
-        <div className='bg-blue-200 my-2'>
+        <div className='flex-column bg-blue-200 my-2 mx-2 border-2 border-black rounded-md'>
             <p>id: {doc.id}</p>
             <p>title: {doc.title}</p>
             <p>body: {doc.body}</p>
