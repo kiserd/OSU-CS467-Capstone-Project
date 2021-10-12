244b40240ddd227b8942d5736c7503cd9a61b2ca
Josh Kaiser <kaiserjo@oregonstate.edu>
2021-10-12 14:32:07 -0400

    Change css component to use module.css

--- portfolio-project-matching-app/components/Button.js
+++ portfolio-project-matching-app/components/Button.js
@@ -1,9 +1,11 @@
+import styles from './Button.module.css';
+
 const Button = ({text, type='btnGeneral', onClick}) => {
     // Types of buttons:
     //      btnGeneral
     //      btnWarning
     return (
-        <button className={type} onClick={onClick}>{text}</button>
+        <button className={styles[type]} onClick={onClick}>{text}</button>
     );
 };
 
