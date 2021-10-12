965feee16b8c31e4de260efb344dc12c4e94e9f8
Josh Kaiser <kaiserjo@oregonstate.edu>
2021-10-12 15:00:47 -0400

    Messing with button style

--- portfolio-project-matching-app/components/Button.js
+++ portfolio-project-matching-app/components/Button.js
@@ -4,6 +4,7 @@ const Button = ({text, type='btnGeneral', onClick}) => {
     // Types of buttons:
     //      btnGeneral
     //      btnWarning
+    //      
     return (
         <button className={styles[type]} onClick={onClick}>{text}</button>
     );
