7717330082914cca90676c5687d34750e847c311
Logan Kiser <kiserd@oregonstate.edu>
2021-10-12 13:36:50 -0700

    wrote and ran basic test on addNewDoc() function in Firebase/clientApp.ts. still need validation and error handling, but it works.

--- portfolio-project-matching-app/Firebase/clientApp.ts
+++ portfolio-project-matching-app/Firebase/clientApp.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, addDoc, setDoc } from 'firebase/firestore';
 // import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,42 @@ const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore();
 // const analytics = getAnalytics(firebaseApp);
 
+
+const getAllDocs = async (collectionName) => {
+  /*
+    DESCRIPTION:  gets all documents from provided collection
+
+    INPUT:        string indicating desired collection name. E.g.,
+                  getAllDocuments('technologies')
+
+    RETURN:       a list of all documents in collection indicated
+  */
+  const coll = collection(db, collectionName);
+  const docSnapshot = await getDocs(coll);
+  const docSnapshotList = docSnapshot.docs.map((doc) => {
+    var arr = doc.data()
+    arr.id = doc.id
+    return arr
+  });
+  return docSnapshotList;
+}
+
+const addNewDoc = async (collectionName, data) => {
+  /*
+    DESCRIPTION:  adds a new document with provided data to the specified
+                  collection. This function does not perform any input
+                  validation - that is currently left to the calling function.
+
+    INPUT:        string indicating desired collection name and data to be
+                  added. E.g.,
+
+                  addNewDoc('technologies', {name: _, language: _, ...})
+    RETURN:       NA
+  */
+  const coll = collection(db, collectionName);
+  const docRef = await addDoc(coll, data);
+  console.log('Document written with ID: ', docRef.id);
+}
+
 // export
-export { db, firebaseApp };
+export { db, firebaseApp, getAllDocs, addNewDoc };
--- portfolio-project-matching-app/components/DBListItem.js
+++ portfolio-project-matching-app/components/DBListItem.js
@@ -1,9 +1,10 @@
 const DBListItem = ({ doc }) => {
     return (
         <div className='flex-column bg-blue-200 my-2 mx-2 border-2 border-black rounded-md'>
-            <p>id: {doc.id}</p>
-            <p>title: {doc.title}</p>
-            <p>body: {doc.body}</p>
+            {Object.keys(doc).sort().map((key) => {
+                console.log(key, doc[key]);
+                return <p>{key}: {doc[key].toString()}</p>
+            })}
         </div>
     )
 }
