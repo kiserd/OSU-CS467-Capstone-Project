1f9e06b1ec3f67287f59c2d5f88c21400d2bd4cb
Logan Kiser <kiserd@oregonstate.edu>
2021-10-13 15:07:01 -0700

    wrapped up the majority of the structure, moving on to responsivivity

---
+++ portfolio-project-matching-app/pages/userIconPage.js
@@ -0,0 +1,11 @@
+import UserIcon from '../components/UserIcon'
+
+const userIconPage = () => {
+    return (
+        <div className='w-full m-10 box-content'>
+            <UserIcon username='username' imgPath='/../public/alien.ico'/>
+        </div>
+    )
+}
+
+export default userIconPage
