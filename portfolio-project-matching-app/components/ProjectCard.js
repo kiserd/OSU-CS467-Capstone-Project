5ada09614abe766318e578e14b8a32a903ff995f
Logan Kiser <kiserd@oregonstate.edu>
2021-10-14 14:58:52 -0700

    fixed left margin to look aligned with buttons (built-in margin)

--- portfolio-project-matching-app/components/ProjectCard.js
+++ portfolio-project-matching-app/components/ProjectCard.js
@@ -1,4 +1,3 @@
-import styles from '../styles/ProjectCard.module.css'
 import Button from '../components/Button'
 import UserIcon from '../components/UserIcon'
 import { doc } from '@firebase/firestore'
@@ -13,19 +12,19 @@ const ProjectCard = ({ project }) => {
     }
     return (
         <div className='p-2 w-full h-full border-2 border-gray-400 rounded-md'>
-            <div className='w-full'>
+            <div className='w-full ml-2'>
                 <p className='text-xl font-medium'>{project.name}</p>
             </div>
-            <hr className='w-11/12 sm:w-9/12 border-b-2 border-gray-400'/>
+            <hr className='w-11/12 sm:w-9/12 ml-2 border-b-2 border-gray-400'/>
             <div className='flex flex-wrap'>
                 {project.technologies.map((technology) => {
                     return <Button key={technology.id} text={technology.name}/>
                 })}
             </div>
-            <div>
+            <div className='ml-2'>
                 <p>{project.description}</p>
             </div>
-            <div className='mt-4'>
+            <div className='mt-4 ml-2'>
                 <p>{project.census} out of {project.capacity} ({project.capacity - project.census} positions left)</p>
             </div>
             <div className='flex flex-wrap'>
@@ -34,7 +33,7 @@ const ProjectCard = ({ project }) => {
                 })}
                 <Button text='join' type='btnGeneral' />
             </div>
-            <div className='inline'>
+            <div className='inline ml-2'>
                 <h3 className='inline'>{project.likes} Likes</h3>
                 <Button text='Like' onClick={likeProject} type='btnGeneral' />
             </div>
