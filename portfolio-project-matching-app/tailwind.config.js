11bd2b9b294d6dfaa8941bd7c6012848ff2cf52a
Logan Kiser <kiserd@oregonstate.edu>
2021-10-15 15:13:40 -0700

    added custom color palette, waiting on teammate input in Slack to proceed

--- portfolio-project-matching-app/tailwind.config.js
+++ portfolio-project-matching-app/tailwind.config.js
@@ -9,8 +9,19 @@ module.exports = {
         'xs': '420px',
         ...defaultTheme.screens,
       },
+      colors: {
+        'custom-cool-extraDark': '#001219',
+        'custom-cool-dark': '#005f73',
+        'custom-cool-med': '#0a9396',
+        'custom-cool-light': '#94d2bd',
+        'custom-cool-extraLight': '#e9d8a6',
+        'custom-warm-extraLight': '#ee9b00',
+        'custom-warm-light': '#ca6702',
+        'custom-warm-med': '#bb3e03',
+        'custom-warm-dark': '#ae2012',
+        'custom-warm-darkExtra': '#9b2226'
+      }
     },
-    // extend: {},
   },
   variants: {
     extend: {},
