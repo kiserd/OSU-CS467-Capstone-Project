2c196706dae171e870ee4b14dd8ef448a0d03687
Logan Kiser <kiserd@oregonstate.edu>
2021-10-15 13:58:31 -0700

    fixed minor typo in my earlier attempt to add additional breakpoint screen size, despite this being the inappropriate branch

--- portfolio-project-matching-app/tailwind.config.js
+++ portfolio-project-matching-app/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    theme: {
+    extend: {
       screens: {
         'xs': '420px',
         ...defaultTheme.screens,
