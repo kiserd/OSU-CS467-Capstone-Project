effa75fa4ee86aecea7bce4901e1b2eac30b0e0a
Logan Kiser <kiserd@oregonstate.edu>
2021-10-15 11:53:25 -0700

    got login component to a decent skeleton presentation

---
+++ portfolio-project-matching-app/components/Login.js
@@ -0,0 +1,42 @@
+import Button from '../components/Button'
+import Input from '../components/Input'
+
+const Login = ({ toggleNewUser }) => {
+    const loginUser = (e) => {
+        // todo
+    }
+
+    return (
+        <div className='w-max p-2 border-2 border-gray-400 rounded-md'>
+            <div className='mx-2 text-2xl text-md'>
+                Login
+            </div>
+            <div className='mx-2 text-sm text-gray-400'>
+                Please login to continue
+            </div>
+            <div className='mt-4'>
+                <label className='mx-2'>
+                    Email
+                </label>
+                <Input type='email' name='email' placeholder='email' />
+            </div>
+            <div className=''>
+                <label className='mx-2'>
+                    Password
+                </label>
+                <Input type='password' name='password' placeholder='password' />
+            </div>
+            <div>
+                <Button type='btnGeneral' text='Login' onClick={loginUser} />
+            </div>
+            <div className='mt-12 text-center text-sm'>
+                Don't have an account?&nbsp;
+                <button className='text-blue-800 font-bold' onClick={toggleNewUser}>
+                    Sign Up
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ portfolio-project-matching-app/pages/test_loginPage.js
@@ -0,0 +1,11 @@
+import Login from '../components/Login'
+
+const test_loginPage = () => {
+    return (
+        <div className='m-12'>
+            <Login />
+        </div>
+    )
+}
+
+export default test_loginPage
