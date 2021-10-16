a8dbb3d2b208979520926ec7882c3f570cb116b9
Logan Kiser <kiserd@oregonstate.edu>
2021-10-16 14:49:09 -0700

    produced skeleton of signup page, still need to fiddle with authentication and firebase database related items

--- portfolio-project-matching-app/components/InputDropdown.js
+++ portfolio-project-matching-app/components/InputDropdown.js
@@ -1,7 +1,7 @@
-const InputDropdown = ({ choices, name }) => {
+const InputDropdown = ({ choices, name, onChange }) => {
     return (
         <div>
-            <select name={name} className='p-2 text-gray-700 border-2 border-gray-200 bg-gray-200 rounded-t-md focus:outline-none hover:border-gray-900 focus:bg-white focus:border-purple-500'>
+            <select name={name} onChange={onChange} className='p-2 text-gray-700 border-2 border-gray-200 bg-gray-200 rounded-t-md focus:outline-none hover:border-gray-900 focus:bg-white focus:border-purple-500'>
                 {choices.map((choice) => {
                     return <option key={choice.id} value={choice.name}>{choice.name}</option>
                 })}
