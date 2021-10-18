697cce2497c17e88ab2b7af96de22dd8f431cbe0
Logan Kiser <kiserd@oregonstate.edu>
2021-10-18 11:09:41 -0700

    built out skeleton, now to work on making the Input component a bit more responsive

---
+++ portfolio-project-matching-app/components/Search.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import Button from '../components/Button'
+import Input from '../components/Input'
+
+const Search = ({ onSearch }) => {
+    const [searchValue, setSearchValue] = useState('')
+
+    const sendSearch = (e) => {
+        // prevent screen from reloading
+        e.preventDefault
+        // if search value is empty string, alert user
+        if (searchValue.length === 0) {
+            alert('please enter search term(s)')
+        }
+        else {
+            onSearch(searchValue)
+            setSearchValue('')
+        }
+    }
+    return (
+        <div className='inline'>
+            <span className='flex-shrink'>
+                <Input type='text' value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>
+            </span>
+            <span className='flex-shrink-0'>
+                <Button text='Search' type='btnGeneral' onClick={sendSearch} />
+            </span>
+        </div>
+    )
+}
+
+export default Search
