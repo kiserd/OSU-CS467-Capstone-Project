29aada9061e746efbd6af3b12af01a507e631e00
Logan Kiser <kiserd@oregonstate.edu>
2021-10-18 14:00:35 -0700

    created skeleton Textarea component

---
+++ portfolio-project-matching-app/pages/test_textareaPage.js
@@ -0,0 +1,11 @@
+import Textarea from '../components/Textarea'
+
+const test_textareaPage = () => {
+    return (
+        <div className='m-12 max-w-lg'>
+            <Textarea placeholder='enter description' numRows={4} />
+        </div>
+    )
+}
+
+export default test_textareaPage
