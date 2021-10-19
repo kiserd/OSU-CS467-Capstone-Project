46fcaa9729f04bb5a820be1fa347645b2b6163f8
Logan Kiser <kiserd@oregonstate.edu>
2021-10-18 17:58:31 -0700

    incorporated addClassName prop into component styling per Josh's review comments

--- portfolio-project-matching-app/components/Textarea.js
+++ portfolio-project-matching-app/components/Textarea.js
@@ -6,7 +6,7 @@ const Textarea = ({ form_id, name, placeholder, numRows, value, addClassName='' 
         placeholder={placeholder}
         rows={numRows}
         value={value}
-        className='w-full p-2 resize-none bg-gray-200 appearance-none border-2 border-gray-200 rounded-md text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'>
+        className={`w-full p-2 resize-none bg-gray-200 appearance-none border-2 border-gray-200 rounded-md text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 ${addClassName}`}>
         </textarea>
     )
 }
