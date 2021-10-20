f8c910df52a42c0733d530955835f85b057ebbf2
Logan Kiser <kiserd@oregonstate.edu>
2021-10-20 09:38:16 -0700

    jumping to fix another issue that browseProjects is dependent upon

--- portfolio-project-matching-app/pages/browseProjects.js
+++ portfolio-project-matching-app/pages/browseProjects.js
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import FilterButtons from '../components/FilterButtons'
 import ProjectCard from '../components/ProjectCard'
 
 const browseProjects = () => {
+
     const projects = [
         {
             name: 'Wastegram',
@@ -16,17 +18,26 @@ const browseProjects = () => {
             users: [{id: 1, username: 'kiserlams'}, {id: 2, username: 'ylijokic'}, {id: 3, username: 'kaiserjo'}]
         },
     ]
+
+    const technologies = [
+        {id: 1, name: 'Javascript'}, {id: 2, name: 'C++'}, {id: 3, name: 'React'}, {id: 4, name: 'Flutter'}
+    ]
     return (
         <div>
-            <span className=''>Search Bar and Add Project</span>
-            <div className='grid grid-cols-3'>
-                <div className='p-2 col-span-2 bg-custom-warm-light'>
+            <div className='p-2 bg-custom-warm-med'>
+                Search Bar and Add Project
+            </div>
+            <div className='p-2 grid grid-cols-3'>
+                <div className='p-2 col-span-2'>
                     {projects.map((project) => {
                         return <ProjectCard project={project} />
                     })}
                 </div>
-                <div className='p-2 col-span-1 bg-custom-cool-light'>
-                    filters
+                <div className='p-2 col-span-1 border-2 border-gray-400 rounded-md'>
+                    <div>
+                        FILTERS
+                    </div>
+                    <FilterButtons category='technologies' choices={technologies} />
                 </div>
             </div>
         </div>
