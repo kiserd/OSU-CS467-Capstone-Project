431a989496b5f10442f0e9511c12fba9c3924cd8
Josh <kaiserjo@oregonstate.edu>
2021-10-21 10:52:26 -0400

    Adds onChange to Textarea component

--- portfolio-project-matching-app/components/Textarea.js
+++ portfolio-project-matching-app/components/Textarea.js
@@ -1,4 +1,4 @@
-const Textarea = ({ form_id, name, placeholder, numRows, value, addClassName='' }) => {
+const Textarea = ({ form_id, name, placeholder, numRows, value, onChange, addClassName='' }) => {
     return (
         <textarea
         form={form_id}
@@ -6,6 +6,7 @@ const Textarea = ({ form_id, name, placeholder, numRows, value, addClassName='' 
         placeholder={placeholder}
         rows={numRows}
         value={value}
+        onChange={onChange}
         className={`w-full p-2 resize-none bg-gray-200 appearance-none border-2 border-gray-200 rounded-md text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 ${addClassName}`}>
         </textarea>
     )
