5ec4f6c4bffcc1f868fef2a3719a451c56cdd287
Josh <kaiserjo@oregonstate.edu>
2021-10-22 10:40:38 -0400

    BCreates way to manage state in myProfilee

--- portfolio-project-matching-app/pages/myProfile.js
+++ portfolio-project-matching-app/pages/myProfile.js
@@ -1,8 +1,64 @@
-const myProfile = () => {
-    return(
-        <div>
+import { useState, useEffect } from 'react'
+import Button from '../components/Button'
+import Input from '../components/Input'
+import Textarea from '../components/Textarea'
 
-        </div>
+
+const myProfile = () => {
+        const initialUserProfileState = {
+            username: "",
+            email: "",
+            timezone: "",
+            introduction: "",
+            technologies: []
+        };
+
+        let [userProfileValues, setUserprofileValues] = useState(initialUserProfileState);
+    
+        useEffect(()=>{
+            getUserProfile();
+        },[]);
+    
+        const getUserProfile = () => {
+            const values = {
+                username: "joshk",
+                email: "a@b.com",
+                timezone: "ET",
+                introduction: "Hi I'm Josh and I like to party",
+                technologies: ['JavaScript', 'Python', 'Blockchain']
+            }
+            // const values =  api.getUserInfo(user)
+    
+            setUserprofileValues({...userProfileValues, ...values})
+        }
+    
+        const handleInputChange = e => {
+            const { name, value } = e.target;
+            console.log(`${name}: ${value}`)
+            setUserprofileValues({...userProfileValues, [name]: value});
+            console.log(`${JSON.stringify(userProfileValues)}`)
+            console.log("input changed")
+        }
+    
+        const handleSubmit = e=> {
+            e.preventDefault();
+            // api.setUserInfo(userProfileValues);
+            console.log("Form submitted");
+        }
+    
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    
+
+                    <label>Introduction:</label>
+                    <Textarea value={userProfileValues.introduction} name="introduction" onChange={handleInputChange}/>
+
+                    
+
+                    <Button text="Submit"/>
+                </form>
+            </div>
     )
 }
 
