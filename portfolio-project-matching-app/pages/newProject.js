b28b70ccae86a76adcdd59c45510881221424057
ylijokic <44416709+ylijokic@users.noreply.github.com>
2021-10-26 10:52:15 -0600

    Add technology input to newProject form

--- portfolio-project-matching-app/pages/newProject.js
+++ portfolio-project-matching-app/pages/newProject.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import Textarea from '../components/Textarea'
@@ -7,24 +7,36 @@ import { addNewDoc } from '../Firebase/clientApp.ts'
 
 const technologies = [{id: 1, name: 'Javascript'}, {id: 2, name: 'C++'}, {id: 3, name: 'React'}, {id: 4, name: 'Flutter'}]
 
-const addProject = async (e) => {
-    e.preventDefault()
+const newProject = () => {
+    const [selectedTechnologies, updateSelectedTechnologies] = useState([]);
+
+    const addProject = async (e) => {
+    e.preventDefault();
+    console.log(e.target.technologies);
     let payload = {
         name: e.target.name.value,
         description: e.target.description.value,
         capacity: e.target.capacity.value,
+        technologies: selectedTechnologies,
         cencus: 1,
         open: true,
         likes: 0
     }
     console.log(payload)
     // await addNewDoc('projects', payload);
-}
+    updateSelectedTechnologies([]);
+    }
 
-const newProject = () => {
+    const update = (a) => {
+        if (selectedTechnologies.includes(a.name) === false) {
+            updateSelectedTechnologies([...selectedTechnologies, a.name])
+        }
+    }
+
     return (
-        <div className='w-full mx-auto max-w-xs border-2 border-gray-400 rounded-md shadow-md'>
-            <div className='p-2 divide-y divide-gray-400'>
+        <div className='bg-gray-200 p-2 w-full h-full'>
+            <div className='w-full mx-auto max-w-xs border-2 border-gray-400 rounded-md shadow-md'>
+                <div className='p-2 divide-y divide-gray-400'>
                     <form onSubmit={addProject}>
                         <div className='mb-4 mt-4'>
                             <label for='name' className='block mb-2'>Project Name:</label>
@@ -32,7 +44,8 @@ const newProject = () => {
                         </div>
                         <div className='mb-4'>
                             <label for='technologies' className='block mb-2'>Technologies:</label>
-                            <FilterButtons choices={technologies} onClick={(a) => console.log(a)}/>
+                            {/* FIX THIS: Temporary Solution. Need more robust way of selecting technologies*/}
+                            <FilterButtons choices={technologies} onClick={update}/>
                         </div>
                         <div className='mb-4'>
                             <label for='description' className='block mb-2'>Description:</label>
@@ -40,12 +53,13 @@ const newProject = () => {
                         </div>
                         <div className='mb-4'>
                             <label for='capacity' className='block mb-2'>Member Capacity:</label>
-                            <Input type={'number'} name={'capacity'} placeholder={'Capacity'} />
+                            <Input type='number' name='capacity' placeholder='Capacity' />
                         </div>
                         <div className='mb-2'>
                             <Button text='Submit'/>
                         </div>
                     </form>
+                </div>
             </div>
         </div>
     )
