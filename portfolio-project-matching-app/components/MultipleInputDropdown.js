f86f7df95af38f68cc946050185dfe20614fc5a8
ylijokic <44416709+ylijokic@users.noreply.github.com>
2021-10-30 14:38:04 -0600

    Add styling to MultipleInputDropdown component

--- portfolio-project-matching-app/components/MultipleInputDropdown.js
+++ portfolio-project-matching-app/components/MultipleInputDropdown.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import Select from 'react-select'
+import styles from './MultipleInputDropdown.module.css'
 
 const MultipleInputDropdown = ({options, onChange}) => {
+    const theme = (theme) => ({
+      ...theme,
+      borderRadius: '0.375rem',
+      colors: {
+        ...theme.colors,
+        primary: '#9f7aea',
+      },
+    })
+
+    const customStyles = {
+      multiValueLabel: (base) => ({
+        ...base,
+        border: '1px solid #9f7aea',
+        borderRadius: '0.375rem',
+      }),
+      multiValueRemove: (base) => ({
+        ...base,
+        backgroundColor: 'white',
+        border: `1px solid red`,
+        borderRadius: '0.375rem',
+        
+      }),
+    }
+
     return (
-        <div>
-            <Select options={options} onChange={onChange} isMulti isClearable isSearchable />
-        </div>
+        <Select options={options} onChange={onChange} isMulti isClearable isSearchable styles={customStyles} theme={theme}/>
     )
 }
 
