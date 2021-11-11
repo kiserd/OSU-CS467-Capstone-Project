462c186d12769fb67ec61fa47a9233cf6126e379
ylijokic <44416709+ylijokic@users.noreply.github.com>
2021-11-10 21:53:47 -0700

    Add color themes for NavBar

--- portfolio-project-matching-app/components/Nav.js
+++ portfolio-project-matching-app/components/Nav.js
@@ -17,7 +17,7 @@ const Nav = () => {
         }
     )
     return (
-        <nav className={styles.nav}>
+        <nav className={styles.navCool}>
             <ul className="grid grid-cols-3">
                 <li className="col-start-2 justify-self-center m-2">
                     <Link href="/">
