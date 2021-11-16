e6aa91926ec4d2f0d0a4e52034737d88bc5a3713
kiserd <kiserd@oregonstate.edu>
2021-11-16 08:48:11 -0800

    added likes field to payload, converted capacity to integer, and fixed census being incremented twice

--- portfolio-project-matching-app/pages/newProject.js
+++ portfolio-project-matching-app/pages/newProject.js
@@ -93,9 +93,10 @@ const newProject = () => {
         // get open field value by examining capacity
         const openStatus = payload.capacity === 1 ? false : true;
         // build on payload object with values from form
-        // there has to be a better way to do this
         const newPayload = payload;
-        newPayload.census = 1;
+        newPayload.census = 0; // createAssociation will increment to 1
+        newPayload.capacity = parseInt(newPayload.capacity, 10)
+        newPayload.likes = 0;
         newPayload.ownerId = authUser.user.id;
         newPayload.open = openStatus
         // set payload statge
